872e13fb17a3d15c3854d68027cda198fc2e382a
Hachim YOUSSOUFA <feihachim@yahoo.fr>
2023-07-16 14:13:55 +0200

    made models of thread and reply

--- models/reply.js
+++ models/reply.js
@@ -1,5 +1,27 @@
 const mongoose = require("mongoose");
 
-const replySchema = new mongoose.Schema({});
+const replySchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  delete_password: {
+    type: String,
+    required: true,
+  },
+  thread_id: {
+    type: mongoose.Schema.ObjectId,
+    ref: "Thread",
+    required: true,
+  },
+  created_on: {
+    type: Date,
+    default: Date.now(),
+  },
+  reported: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 module.exports = mongoose.model("Reply", replySchema);
--- models/thread.js
+++ models/thread.js
@@ -1,5 +1,31 @@
 const mongoose = require("mongoose");
 
-const threadSchema = new mongoose.Schema({});
+const threadSchema = new mongoose.Schema({
+  text: {
+    type: String,
+    required: true,
+  },
+  delete_password: {
+    type: String,
+    required: true,
+  },
+  created_on: {
+    type: Date,
+    default: Date.now(),
+  },
+  bumped_on: {
+    type: Date,
+    default: Date.now(),
+  },
+  reported: {
+    type: Boolean,
+    default: false,
+  },
+  replies: {
+    type: [mongoose.Schema.ObjectId],
+    ref: "Reply",
+    default: [],
+  },
+});
 
 module.exports = mongoose.model("Thread", threadSchema);
